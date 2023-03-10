2e91a5bfe045f7465edd6f7f4259b918b2e7c6e4
Ben Tryon <b.t.tryon@gmail.com>
2023-03-10 11:54:07 -0700

    added About text

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -3,10 +3,37 @@ import Navbar from './Navbar';
 
 function About() {
     return (
-        <div>
-        <p>The About Page</p>
+      <div>
+        <p>
+          Yellow is the color of hope, of happiness, of the sun... which will
+          eventually blow up and kill us all. Just like Jeff Bezos and Amazon.
+        </p>
+        <p>
+          Our main competitor, who shall remain unnamed (but rhymes with
+          Gudreeds), was purchased by Amazon as it expanded. Now someone who
+          wants the perfect book to follow-up One Hundred Years of Solitude will
+          not get a legitimate recommendation. People like their books to be
+          different, even if you're still in the same genre. GoodReads says your
+          next book should be the same book. Maybe the author changed, but
+          everything else is exactly the same. Then when you're done with OHYoS,
+          AmazonReads recommends more of Gabe's books. An you be like, 'yeh,
+          more of the same'.... but here
+        </p>
+        <h6>at the Unicorn</h6>
+        <p>
+          we have a better idea... three of the sameish qualites of a book,
+          match with 3 of the sameish qualities of <em>another</em> book... and
+          you get a recommendation based on the <strong>wildcard</strong> that
+          changes your life forever.
+        </p>
+        <p>
+          So instead of read-reading OHYoS, you see Good Night Moon matched with
+          3 qualities and you get "The Little Prince" because the OHYoS and Moon
+          are magically realistic, bildungsroman, that paint amazing landscapes in your head or real life... and the "Little Prince" has each of those qualities... but is also wild
+          
+        </p>
         <Navbar />
-        </div>
-    )
+      </div>
+    );
 }
 export default About;
